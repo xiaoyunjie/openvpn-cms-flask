e81cb953a3426ba41184152e9017fb7c2c3a5bb9
jack.xiao <Browser_hot@163.com>
2020-07-30 15:59:51 +0800

    解决SQLAlchemy长时间未请求数据库连接断开

--- app/config/secure.py
+++ app/config/secure.py
@@ -16,6 +16,9 @@ class DevelopmentSecure(BaseConfig):
 
     SQLALCHEMY_ECHO = False
 
+    # 对线程池中的线程进行一次连接的回收，如果这个值是-1代表永不回收
+    SQLALCHEMY_POOL_RECYCLE = 1200
+
     SECRET_KEY = '\x88W\xf09\x91\x07\x98\x89\x87\x96\xa0A\xc68\xf9\xecJJU\x17\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x95*4'
 
 
@@ -27,5 +30,8 @@ class ProductionSecure(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:Gepoint@localhost:3306/openvpn'
 
     SQLALCHEMY_ECHO = False
+
+    # 对线程池中的线程进行一次连接的回收，如果这个值是-1代表永不回收
+    SQLALCHEMY_POOL_RECYCLE = 1200
 
     SECRET_KEY = '\x88W\xf09\x91\x07\x98\x89\x87\x96\xa0A\xc68\xf9\xecJJU\x17\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x95*4'
