80292e07f7e5d3d28b3ce3c50c6e7e11ddb9190a
jack.xiao <Browser_hot@163.com>
2020-12-07 14:11:31 +0800

    New configuration reference

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@
     :license: MIT, see LICENSE for more details.
 """
 
-
+"""
+    :初始参数设置
+    :vpn服务器地址 VPN_ADDRESS
+    :vpn服务器端口 VPN_PORT
+"""
 VPN_ADDRESS = '192.168.149.9'
 VPN_PORT = '11940'
